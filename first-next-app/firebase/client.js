0f1aacd3df51ee0671ba02e837ac9ada58ef0e5e
Luvy <71282027+Luvy-js@users.noreply.github.com>
2021-05-30 23:47:30 +0200

    Add basic firebase config

---
+++ first-next-app/firebase/client.js
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA6Mwso6NcYuS82RQGFmk2P88VIvxfYB8A",
+  authDomain: "next-js-twitter-clone.firebaseapp.com",
+  projectId: "next-js-twitter-clone",
+  storageBucket: "next-js-twitter-clone.appspot.com",
+  messagingSenderId: "836225459397",
+  appId: "1:836225459397:web:b283b290807033ef0cfcac",
+  measurementId: "G-PE8X1K87K5"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+export const loginWithGitHub = () => {
+  const gitHubProvider = new.firebase.auth.gitHubProvider()
+  return firebase.auth().signInWithPopup(gitHubProvider)
+}
--- first-next-app/pages/index.js
+++ first-next-app/pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import Link from 'next/link'
 import colors from '../styles/theme'
 import Button from '../components/Button'
 
