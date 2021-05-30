c1507db410edca878d753c4b23d0c12afea4f6ee
Luvy <71282027+Luvy-js@users.noreply.github.com>
2021-05-30 17:30:20 +0200

    Add styles to main in every page

--- first-next-app/pages/detail/index.js
+++ first-next-app/pages/detail/index.js
@@ -4,15 +4,18 @@ import styles from '../../styles/Home.module.css'
 export default function Detail({ userName, data }){
   return ( 
     <div className={styles.container}>
-      <h1>{userName}'s detail page</h1> 
-      <p>{data}</p>
-      <Link href="/" >
-          Home
-      </Link>
-      <br />
-      <Link href="/timeline" >
-          Timeline
-      </Link>
+      
+      <main className={styles.main}>
+        <h1>{userName}'s detail page</h1> 
+        <p>{data}</p>
+        <Link href="/" >
+            Home
+        </Link>
+        <br />
+        <Link href="/timeline" >
+            Timeline
+        </Link>
+      </main>
     </div>
   )
 }
